fb72248547265754e2179fd72448d490765b914d
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:07:36 +0000

    lint Regenerate Role

--- dist/creep.role.regenerate.js
+++ dist/creep.role.regenerate.js
@@ -1,19 +1,19 @@
 var utils = require('utils')
 
 function regenerate(creep) {
-    if (creep.ticksToLive < 1500 * 0.90) {
-        var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
-        if(spawn) {
-            if(spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn)
-            }
-        } else {
-            utils.go_relax(creep)
-        }
+  if (creep.ticksToLive < 1500 * 0.90) {
+    var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
+    if(spawn) {
+      if(spawn.renewCreep(creep) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(spawn)
+      }
     } else {
-        // feeling spritely again!
-        delete creep.memory.role
+      utils.go_relax(creep)
     }
+  } else {
+        // feeling spritely again!
+    delete creep.memory.role
+  }
 }
 
 module.exports = regenerate
