1ef3291b6440c564f68a4055aaf26ff3eacc19db
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 21:50:59 +0000

    Stop harvesters trying to collect dropped energy

--- dist/creep.role.harvester.js
+++ dist/creep.role.harvester.js
@@ -9,9 +9,6 @@ if (!Memory.population) {
 }
 
 function harvester(creep) {
-  // creep.say(
-  //   creep.memory.transferring === true ? 'Transferring' : 'Harvesting'
-  // )
   if(creep.memory.state && creep.carry.energy == 0) {
     creep.memory.state = false
   }
@@ -70,21 +67,7 @@ function harvester(creep) {
     }
   }
   else {
-    if (!creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)) {
-      var free_energy = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY)
-      if (free_energy!= null && free_energy.resourceType == 'energy') {
-        if(creep.pickup(free_energy) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(free_energy)
-        }
-      }
-      else {
-
-        utils.harvest_nearest_energy(creep)
-      }
-    }
-    else {
-      utils.harvest_nearest_energy(creep)
-    }
+    utils.harvest_nearest_energy(creep)
   }
 }
 
