4870ff15cae583201d267aa85548d3c943d2a029
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:06:59 +0000

    Lint Upgrader Role

--- dist/creep.role.upgrader.js
+++ dist/creep.role.upgrader.js
@@ -1,30 +1,30 @@
 var utils = require('utils')
 
 if (!Memory.population) {
-    Memory.population = {
-        upgrader: 2,
-    }
+  Memory.population = {
+    upgrader: 2,
+  }
 } else if (!Memory.population.upgrader) {
-    Memory.population.upgrader = 2
+  Memory.population.upgrader = 2
 }
 
 function upgrader(creep) {
 
-    if(creep.memory.upgrading && creep.carry.energy == 0) {
-        creep.memory.upgrading = false
-    }
-    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-        creep.memory.upgrading = true
-    }
+  if(creep.memory.upgrading && creep.carry.energy == 0) {
+    creep.memory.upgrading = false
+  }
+  if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+    creep.memory.upgrading = true
+  }
 
-    if(creep.memory.upgrading) {
-        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.room.controller)
-        }
+  if(creep.memory.upgrading) {
+    if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(creep.room.controller)
     }
-    else {
-        utils.collect_nearest_energy(creep)
-    }
+  }
+  else {
+    utils.collect_nearest_energy(creep)
+  }
 }
 
 module.exports = upgrader
