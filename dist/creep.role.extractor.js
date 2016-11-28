c2444e030f0daec3edc5987691ca6c46cb424e1d
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:10:56 +0000

    Lint Extractor Role

--- dist/creep.role.extractor.js
+++ dist/creep.role.extractor.js
@@ -1,16 +1,18 @@
 var utils = require('utils')
 
 if (!Memory.population) {
-    Memory.population = {
-        extractor: 0,
-    }
+  Memory.population = {
+    extractor: 0,
+  }
 } else if (!Memory.population.extractor) {
-    Memory.population.extractor = 0
+  Memory.population.extractor = 0
 }
 
 function extractor(creep) {
-    // creep.say(creep.memory.transferring === true ? 'Transferring' : 'Harvesting')
-    utils.harvest_nearest_energy(creep)
+    // creep.say(
+    //   creep.memory.transferring === true ? 'Transferring' : 'Harvesting'
+    // )
+  utils.harvest_nearest_energy(creep)
 }
 
 module.exports = extractor
