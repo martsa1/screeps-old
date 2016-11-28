fd414e3fba3c327475a35c19fc2c23b9b09f6904
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:05:57 +0000

    Lint Main

--- dist/main.js
+++ dist/main.js
@@ -5,14 +5,14 @@ var towerManager = require('towerManager')
 module.exports.loop = function () {
 
     // Manage creeps room by room
-    for (var room in Game.rooms) {
-        var current_room = Game.rooms[room]
-        creepManager.manage(current_room)
-    }
+  for (var room in Game.rooms) {
+    var current_room = Game.rooms[room]
+    creepManager.manage(current_room)
+  }
 
     // Run creeps globally
-    creepManager.run()
+  creepManager.run()
 
     // Towers don't need to be run by room though...
-    towerManager.manage()
+  towerManager.manage()
 }
