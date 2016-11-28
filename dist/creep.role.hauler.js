7dc52f93c901eac12e2b049daf9e6d48e8081e22
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 22:39:08 +0000

    Initial (Very) primitive hauler creep
    Does a bit of distributing, needs much more attention.

--- dist/creep.role.hauler.js
+++ dist/creep.role.hauler.js
@@ -17,22 +17,31 @@ function hauler(creep) {
   }
 
   if (!creep.memory.state) {
-    var target = get_full_extractor(creep)
-    if (target) {
-      console.log(JSON.stringify(target))
-      if (target.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        moveTo(target)
+    var source = utils.get_full_extractor(creep)
+    if (source) {
+      console.log(JSON.stringify(source))
+      if (source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source)
+      }
+    }
+    else {
+      source = utils.find_nearest_energy_collection_point(creep)
+      if (source && (source.structureType !== STRUCTURE_SPAWN
+          || source.structureType !== STRUCTURE_EXTENSION)) {
+        if (source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source)
+        }
       }
     }
   }
 
   if (creep.memory.state){
-    var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
       filter: (structure) => {
         return (
-          (structure.structureType == STRUCTURE_TOWER)
-          && structure.energy < structure.energyCapacity
-        )
+          (structure.structureType == STRUCTURE_EXTENSION
+          || structure.structureType == STRUCTURE_SPAWN)
+          && structure.energy < structure.energyCapacity)
       }
     })
     if(target) {
@@ -44,9 +53,9 @@ function hauler(creep) {
       var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType == STRUCTURE_EXTENSION
-            || structure.structureType == STRUCTURE_SPAWN)
-            && structure.energy < structure.energyCapacity)
+            (structure.structureType == STRUCTURE_TOWER)
+            && structure.energy < structure.energyCapacity
+          )
         }
       })
       if(target) {
@@ -58,35 +67,6 @@ function hauler(creep) {
         creep.moveTo(Game.flags.Chillout)
       }
     }
-
-  }
-}
-
-function get_storage(creep) {
-    // Returns any storag/container units in the room
-  return storage_unit = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-    filter: (structure) => {
-      return (structure.structureType == STRUCTURE_STORAGE ||
-                structure.structureType == STRUCTURE_CONTAINER) &&
-                structure.store[RESOURCE_ENERGY] > 0
-    }
-  })
-
-}
-
-function get_full_extractor(creep) {
-  var source = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-    filter: (extractor) => {
-      extractor.memory.role == 'harvester' &&
-            extractor.carry[RESOURCE_ENERGY] > 0
-    }
-  })
-  var fullest = _.max(source)
-  if (fullest == '-Infinity') {
-    return
-  } else {
-    console.log('Returning Fullest creep!')
-    return fullest
   }
 }
 
