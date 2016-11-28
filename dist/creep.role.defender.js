02fea4af680a4ca4eb729daf3ab524fa1c27a540
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:10:47 +0000

    Lint Defender Code

--- dist/creep.role.defender.js
+++ dist/creep.role.defender.js
@@ -1,25 +1,25 @@
 var utils = require('utils')
 
 if (!Memory.population) {
-    Memory.population = {
-        defender: 0,
-    }
+  Memory.population = {
+    defender: 0,
+  }
 } else if (!Memory.population.defender) {
-    Memory.population.defender = 0
+  Memory.population.defender = 0
 }
 
 function defender(creep) {
-    var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
+  var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
 
-    if(target) {
-        console.log ('Found an Invader!')
-        if(creep.rangedAttack(target) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target)
-        }
+  if(target) {
+    console.log ('Found an Invader!')
+    if(creep.rangedAttack(target) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
     }
-    else {
-        utils.go_relax(creep)
-    }
+  }
+  else {
+    utils.go_relax(creep)
+  }
 }
 
 module.exports = defender
