eb717f0887f566dad0aadbfeb5532450ca6f86f7
Sam Martin <Sam.Martin@Net2Edge.com>
2016-11-28 12:10:42 +0000

    Lint Builder Role

--- dist/creep.role.builder.js
+++ dist/creep.role.builder.js
@@ -1,59 +1,66 @@
 var utils = require('utils')
 
 if (!Memory.population) {
-    Memory.population = {
-        builder: 2,
-    }
+  Memory.population = {
+    builder: 2,
+  }
 } else if (!Memory.population.builder) {
-    Memory.population['builder'] = 2
+  Memory.population['builder'] = 2
 }
 
 function builder(creep) {
+  if(creep.memory.state && creep.carry.energy == 0) {
+    creep.memory.state = false
+  }
+  if(!creep.memory.state && creep.carry.energy == creep.carryCapacity) {
+    creep.memory.state = true
+  }
 
-    if(creep.memory.state && creep.carry.energy == 0) {
-        creep.memory.state = false
-    }
-    if(!creep.memory.state && creep.carry.energy == creep.carryCapacity) {
-        creep.memory.state = true
-    }
+  var target = {}
 
-    if(creep.memory.state) {
-        var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
-        if(target) {
-            if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target)
-            }
-        }
-        else {
-            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax * 0.95 &&
-                    structure.structureType != STRUCTURE_WALL &&
-                    structure.structureType != STRUCTURE_RAMPART
-            })
-            if(target) {
-                if(creep.repair(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target)
-                }
-            } else {
-                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits <= Memory.defense.wall_health &&
-                        (structure.structureType == STRUCTURE_WALL ||
-                         structure.structureType == STRUCTURE_RAMPART)
-                })
-                if(target) {
-                    if(creep.repair(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target)
-                    }
-                } else {
-                    utils.go_relax(creep)
-                }
-            }
-
-        }
+  if(creep.memory.state) {
+    target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
+    if(target) {
+      if(creep.build(target) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target)
+      }
     }
     else {
-        utils.collect_nearest_energy(creep)
+      target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => (
+          structure.hits < structure.hitsMax * 0.95 &&
+          structure.structureType != STRUCTURE_WALL &&
+          structure.structureType != STRUCTURE_RAMPART
+        )
+      })
+      if(target) {
+        if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target)
+        }
+      }
+      else {
+        target = creep.pos.findClosestByRange(FIND_STRUCTURES,
+          {
+            filter: (structure) => (
+              structure.hits <= Memory.defense.wall_health &&
+              (structure.structureType == STRUCTURE_WALL ||
+              structure.structureType == STRUCTURE_RAMPART)
+            )
+          }
+        )
+        if(target) {
+          if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target)
+          }
+        } else {
+          utils.go_relax(creep)
+        }
+      }
     }
+  }
+  else {
+    utils.collect_nearest_energy(creep)
+  }
 }
 
 module.exports = builder
