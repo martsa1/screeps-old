bf584916e14cfd9681e5689c6ce0fe96ab7516f0
Sam Martin <Sam.Martin@Net2Edge.com>
2016-12-02 08:54:27 +0000

    Add beginnings of room manager

---
+++ dist/room.manager.js
@@ -0,0 +1,8 @@
+var createJobsList = function() {
+  // Get a reference to the room we are working in
+  // var room = this
+
+  // console.log('Room Object:', JSON.stringify(room.controller))
+}
+
+module.exports = createJobsList
