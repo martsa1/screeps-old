310a6cf51d42c36b596548da84ce13a98bf47a33
Sam Martin <Sam.Martin@net2edge.com>
2016-12-06 10:06:33 +0000

    Update hauler to use room allocated jobs, deprecate old functions

--- dist/creep.role.hauler.js
+++ dist/creep.role.hauler.js
@@ -1,6 +1,6 @@
 var utils = require('utils')
 
-function hauler(creep, room) {
+function hauler(creep) {
   // console.log(JSON.stringify(creep.memory))
   if (!creep.memory.state || creep.memory.state === '') {
     creep.memory['state'] = 'refill'
@@ -14,20 +14,19 @@ function hauler(creep, room) {
   }
 
   if (creep.memory.state === 'refill') {
-    if (!creep.memory.targetID) {
+    if (!creep.memory.allocation) {
       console.log(creep.name,
-                  'I don\'t have a source to work with, looking for one!')
-      get_extractor_buddy(creep, room)
+                  'I don\'t have a source to work with!')
+      utils.go_relax(creep)
+      // get_extractor_buddy(creep, room)
     }
     else {
-      // console.log(creep.name, 'collecting energy from',
-      //             creep.memory.targetID)
-      collect_energy_by_targetID(creep, room)
+      collect_energy_by_allocation(creep)
     }
   }
 
   if (creep.memory.state === 'transport'){
-    delete creep.memory.targetID
+    delete creep.memory.allocation
     target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
       filter: (structure) => {
         return (
@@ -62,71 +61,71 @@ function hauler(creep, room) {
   }
 }
 
-function get_extractor_buddy(creep, room) {
-  console.log('Finding a source');
-  var haulers = room.find(FIND_MY_CREEPS, {
-    filter: function(unit) {
-      return unit.memory.role == 'hauler'
-    }
-  })
+// function get_extractor_buddy(creep, room) {
+//   console.log('Finding a source');
+//   var haulers = room.find(FIND_MY_CREEPS, {
+//     filter: function(unit) {
+//       return unit.memory.role == 'hauler'
+//     }
+//   })
+//
+//   var sources = room.find(FIND_DROPPED_ENERGY,  {
+//     filter: function(energy) {
+//       return energy.amount >= creep.carryCapacity * 0.5
+//     }
+//   })
+//   sources = sources.concat(room.find(FIND_STRUCTURES,  {
+//     filter: function(structure) {
+//       return structure.structureType == STRUCTURE_CONTAINER
+//     }
+//   }))
+//   // console.log(JSON.stringify(sources));
+//   if (sources.length > 0){
+//     sources = sources.map(({ id }) => id)
+//       .reduce((former, next) => {
+//         // console.log(former, next);
+//         var alreadyAllocated = false
+//         for (let name in haulers) {
+//           let otherCreep = haulers[name]
+//           if (otherCreep.name === creep.name) {
+//             continue
+//           }
+//           // console.log(otherCreep.name, ', memory:',
+//           //   JSON.stringify(otherCreep.memory))
+//           if (otherCreep.memory.targetID
+//               && otherCreep.memory.targetID === former) {
+//             alreadyAllocated = true
+//             console.log('Found a source allocated to someone else, ignoring')
+//             break
+//           }
+//           if (!alreadyAllocated) {
+//             return former
+//           }
+//           else {
+//             console.log('Found an unallocated source')
+//             return next
+//           }
+//         }
+//       })
+//     if (sources) {
+//       creep.memory.targetID = sources
+//       console.log(JSON.stringify(sources))
+//       console.log(creep.name, 'found a source:', creep.memory.targetID);
+//     }
+//   }
+//   else {
+//     console.log(JSON.stringify(sources))
+//     console.log(creep.name, 'can\'t find a source!')
+//     utils.go_relax(creep)
+//   }
+// }
 
-  var sources = room.find(FIND_DROPPED_ENERGY,  {
-    filter: function(energy) {
-      return energy.amount >= creep.carryCapacity * 0.5
-    }
-  })
-  sources = sources.concat(room.find(FIND_STRUCTURES,  {
-    filter: function(structure) {
-      return structure.structureType == STRUCTURE_CONTAINER
-    }
-  }))
-  // console.log(JSON.stringify(sources));
-  if (sources.length > 0){
-    sources = sources.map(({ id }) => id)
-      .reduce((former, next) => {
-        // console.log(former, next);
-        var alreadyAllocated = false
-        for (let name in haulers) {
-          let otherCreep = haulers[name]
-          if (otherCreep.name === creep.name) {
-            continue
-          }
-          // console.log(otherCreep.name, ', memory:',
-          //   JSON.stringify(otherCreep.memory))
-          if (otherCreep.memory.targetID
-              && otherCreep.memory.targetID === former) {
-            alreadyAllocated = true
-            console.log('Found a source allocated to someone else, ignoring')
-            break
-          }
-          if (!alreadyAllocated) {
-            return former
-          }
-          else {
-            console.log('Found an unallocated source')
-            return next
-          }
-        }
-      })
-    if (sources) {
-      creep.memory.targetID = sources
-      console.log(JSON.stringify(sources))
-      console.log(creep.name, 'found a source:', creep.memory.targetID);
-    }
-  }
-  else {
-    console.log(JSON.stringify(sources))
-    console.log(creep.name, 'can\'t find a source!')
-    utils.go_relax(creep)
-  }
-}
-
-function collect_energy_by_targetID(creep) {
-  var source = Game.getObjectById(creep.memory.targetID)
+function collect_energy_by_allocation(creep) {
+  var source = Game.getObjectById(creep.memory.allocation)
   if (!source) {
     console.log('I seem to have forgotten my destination as it doesn\'t seem '
       + 'to make sense anymore')
-    delete creep.memory.targetID
+    delete creep.memory.allocation
     return
   }
   if (source.resourceType == RESOURCE_ENERGY){
