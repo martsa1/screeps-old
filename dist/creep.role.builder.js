c127ec5b7549c5bb69c854c05f5ba7a66cc0e459
Sam Martin <Sam.Martin@net2edge.com>
2016-12-06 10:07:12 +0000

    Update builder role to use room allocated jobs, deprecate old functions

--- dist/creep.role.builder.js
+++ dist/creep.role.builder.js
@@ -1,54 +1,64 @@
 var utils = require('utils')
 
 function builder(creep) {
-  if(creep.memory.state && creep.carry.energy == 0) {
-    creep.memory.state = false
+  if (!creep.memory.state) {
+    creep.memory.state = 'refill'
   }
-  if(!creep.memory.state && creep.carry.energy == creep.carryCapacity) {
-    creep.memory.state = true
+  if(creep.memory.state === 'build' && creep.carry.energy == 0) {
+    creep.memory.state = 'refill'
+  }
+  if(creep.memory.state === 'refill'
+     && creep.carry.energy == creep.carryCapacity) {
+    creep.memory.state = 'build'
   }
 
   var target = {}
 
-  if(creep.memory.state) {
-    target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
+  if(creep.memory.state === 'build' && creep.memory.allocation) {
+    target = Game.getObjectById(creep.memory.allocation)
     if(target) {
       if(creep.build(target) == ERR_NOT_IN_RANGE) {
         creep.moveTo(target)
       }
     }
     else {
-      target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (structure) => (
-          structure.hits < structure.hitsMax * 0.95 &&
-          structure.structureType != STRUCTURE_WALL &&
-          structure.structureType != STRUCTURE_RAMPART
-        )
-      })
-      if(target) {
-        if(creep.repair(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target)
-        }
-      }
-      else {
-        target = creep.pos.findClosestByRange(FIND_STRUCTURES,
-          {
-            filter: (structure) => (
-              structure.hits <= Memory.defense.wall_health &&
-              (structure.structureType == STRUCTURE_WALL ||
-              structure.structureType == STRUCTURE_RAMPART)
-            )
-          }
-        )
-        if(target) {
-          if(creep.repair(target) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target)
-          }
-        } else {
-          utils.go_relax(creep)
-        }
-      }
+      console.log(creep.name, 'Don\'t appear to have an allocation.');
+      delete creep.memory.allocation
+      utils.go_relax(creep)
     }
+    // target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
+    // else {
+    //   target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+    //     filter: (structure) => (
+    //       structure.hits < structure.hitsMax * 0.95 &&
+    //       structure.structureType != STRUCTURE_WALL &&
+    //       structure.structureType != STRUCTURE_RAMPART
+    //     )
+    //   })
+    //   if(target) {
+    //     if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+    //       creep.moveTo(target)
+    //     }
+    //   }
+    //   else {
+    //     target = creep.pos.findClosestByRange(FIND_STRUCTURES,
+    //       {
+    //         filter: (structure) => (
+    //           structure.hits <= Memory.defense.wall_health &&
+    //           (structure.structureType == STRUCTURE_WALL ||
+    //           structure.structureType == STRUCTURE_RAMPART)
+    //         )
+    //       }
+    //     )
+    //     if(target) {
+    //       if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+    //         creep.moveTo(target)
+    //       }
+    //     } else {
+    //       utils.go_relax(creep)
+    //     }
+    //   }
+    // }
   }
   else {
     utils.collect_nearest_energy(creep)
